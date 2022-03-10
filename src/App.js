c452e934fb77d8446a404276f4917f11358239b6
d-rigel <alozie4God@gmail.com>
2022-03-10 09:02:59 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import "./responsive.css";
+
 import { HomeScreen } from "./screens/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <HomeScreen />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={HomeScreen} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Header } from "../components/header/Header";
 
 export const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
