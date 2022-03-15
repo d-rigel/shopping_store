aa37394323167332a8079e0317a815ff75cb7058
d-rigel <alozie4God@gmail.com>
2022-03-15 07:58:50 -0700

    action center done!

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Header } from "../components/header/Header";
 import { ShopSection } from "../components/homeComponents/ShopSection";
+import { CallToActionSection } from "../components/homeComponents/CallToActionSection";
+
 export const HomeScreen = () => {
   return (
     <div>
       <Header />
       <ShopSection />
+      <CallToActionSection />
     </div>
   );
 };
