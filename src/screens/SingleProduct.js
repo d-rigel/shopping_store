9fd5c433c067769e7d3276b75c9fc8166151c4be
d-rigel <alozie4God@gmail.com>
2022-03-15 00:12:56 -0700

    completed single product page

--- src/screens/SingleProduct.js
+++ src/screens/SingleProduct.js
@@ -51,7 +51,7 @@ export const SingleProduct = () => {
         </div>
 
         {/* RATINGS */}
-        <div className="">
+        <div className="row my-5">
           <div className="col-md-6">
             <h6 className="mb-6">REVIEW</h6>
             <Message variant={"alert-info mt-3"}>No Reviews</Message>
@@ -59,7 +59,50 @@ export const SingleProduct = () => {
               <strong>Admin Rigel</strong>
               <Rating />
               <span>February 18th 2022</span>
-              <div className=""></div>
+              <div className="alert alert-info mt-3">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <h6>WRITE A CUSTOMER REVIEW</h6>
+            <div className="my-4">
+              <form>
+                <div className="my-4">
+                  <strong>Rating</strong>
+                  <select className="col-12 bg-light p-3 mt-2 border-0 rounded">
+                    <option value="">Select...</option>
+                    <option value="1">1 - Poor</option>
+                    <option value="2">2 - Fair</option>
+                    <option value="3">3 - Good</option>
+                    <option value="4">4 - Very Good</option>
+                    <option value="5">5 - Excellent</option>
+                  </select>
+                </div>
+                <div className="my-4">
+                  <strong>Comment</strong>
+                  <textarea
+                    row="3"
+                    className="col-12 bg-light p-3 mt-2 border-0 rounded"></textarea>
+                </div>
+                <div className="my-3">
+                  <button className="col-12 bg-black border-0 p-3 rounded text-white">
+                    SUBMIT
+                  </button>
+                </div>
+              </form>
+              <div className="my-3">
+                <Message variant={"alert-warning"}>
+                  Please{" "}
+                  {/* <Link to="/login">
+                  " <strong>Login</strong> "
+                </Link>{" "} */}
+                  login to write a review{" "}
+                </Message>
+              </div>
             </div>
           </div>
         </div>
