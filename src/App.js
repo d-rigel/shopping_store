3faffd1148085b7fbae807420be06625c52a9b18
d-rigel <alozie4God@gmail.com>
2022-03-18 06:34:22 -0700

    register component done!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HomeScreen } from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SingleProduct } from "./screens/SingleProduct";
 import { Login } from "./screens/Login";
+import { Register } from "./screens/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" component={HomeScreen} exact />
         <Route path="/products/:id" component={SingleProduct} />
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     </Router>
   );
