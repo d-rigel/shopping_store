4282d00ccde8efe78d58b3b2e824d715a49720df
d-rigel <alozie4God@gmail.com>
2022-03-21 12:30:48 -0700

    profile screen started

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SingleProduct } from "./screens/SingleProduct";
 import { Login } from "./screens/Login";
 import { Register } from "./screens/Register";
+import { ProfileScreen } from "./screens/ProfileScreen";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/products/:id" component={SingleProduct} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/Profile" component={ProfileScreen} />
       </Switch>
     </Router>
   );
